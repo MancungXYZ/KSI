aad90d0bd75f2d749e7b479dc6b1f4b58e0e09ec
Mancung <reihanwiyanda@gmail.com>
2023-05-06 16:04:07 +0700

    Keamanan sistem informasi

---
+++ src/ksi/KSI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ksi;
+
+/**
+ *
+ * @author Reihan Wiyanda
+ */
+public class KSI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Memanggil frame utama
+        futama f_utama = new futama();
+        f_utama.setVisible(true);
+    }
+    
+}
